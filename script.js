bd117fb16ba7a264fe9a9b607639b5a957744ef1
Maria Ascolese <99254839+maria-ascolese@users.noreply.github.com>
2023-10-30 17:38:56 +0100

    gradient

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     let navLinks = document.querySelectorAll('nav a');
+    const colors = ['#E6A9F0', '#4C99BF', '#74FF71', '#FDE674'];
     
-    navLinks.forEach(function(link) {
+    navLinks.forEach(function(link, index) {
         link.addEventListener('click', function(e) {
             e.preventDefault();
 
-            navLinks.forEach(function(innerLink) {
-                innerLink.classList.remove('active');
-            });
+            let newGradientColors = [...colors];
+            let mainColor = colors[index];
 
-            link.classList.add('active');
-            
-            document.body.style.backgroundColor = window.getComputedStyle(e.target).backgroundColor;
+            if (index === 0) {
+                newGradientColors[0] += " 70%";
+            } else if (index === colors.length - 1) {
+                newGradientColors[colors.length - 1] = "30% " + newGradientColors[colors.length - 1];
+            } else {
+                newGradientColors.splice(index, 0, mainColor + " 70%");
+            }
+
+            let newGradient = `linear-gradient(to right, ${newGradientColors.join(', ')})`;
+
+            document.body.style.background = newGradient;
 
             document.querySelectorAll('section').forEach(function(sec) {
-                sec.classList.remove('active');
+                sec.style.display = "none";
             });
 
             let targetSection = document.querySelector(e.target.getAttribute('href'));
             if (targetSection) {
-                targetSection.classList.add('active');
+                targetSection.style.display = "block";
             }
         });
     });
