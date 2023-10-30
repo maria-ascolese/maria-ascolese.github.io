018853766cdca6ffb0e012319ce909a3452b1db4
Maria Ascolese <99254839+maria-ascolese@users.noreply.github.com>
2023-10-30 16:36:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,22 @@ document.addEventListener('DOMContentLoaded', function() {
     navLinks.forEach(function(link) {
         link.addEventListener('click', function(e) {
             e.preventDefault();
+
+            navLinks.forEach(function(innerLink) {
+                innerLink.classList.remove('active');
+            });
+
+            link.classList.add('active');
             
             document.body.style.backgroundColor = window.getComputedStyle(e.target).backgroundColor;
-            
+
             document.querySelectorAll('section').forEach(function(sec) {
-                sec.style.display = "none";
+                sec.classList.remove('active');
             });
 
             let targetSection = document.querySelector(e.target.getAttribute('href'));
             if (targetSection) {
-                targetSection.style.display = "block";
+                targetSection.classList.add('active');
             }
         });
     });
