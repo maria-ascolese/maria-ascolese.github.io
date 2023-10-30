e7a365289cde3804e8d7731a64c5624e68e11f6e
Maria Ascolese <99254839+maria-ascolese@users.noreply.github.com>
2023-10-30 16:17:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
         link.addEventListener('click', function(e) {
             e.preventDefault();
             
-            // Change body background color to match the clicked bubble
             document.body.style.backgroundColor = window.getComputedStyle(e.target).backgroundColor;
             
-            // Hide all sections
             document.querySelectorAll('section').forEach(function(sec) {
                 sec.style.display = "none";
             });
 
-            // Display the clicked section
             let targetSection = document.querySelector(e.target.getAttribute('href'));
             if (targetSection) {
                 targetSection.style.display = "block";
